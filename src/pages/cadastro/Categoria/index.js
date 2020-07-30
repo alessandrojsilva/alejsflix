61bfb46f34469e073c29b807706c84f000c5192a
alessandrojsilva <67442986+alessandrojsilva@users.noreply.github.com>
2020-07-30 01:33:55 -0300

    final aula 3

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,23 +1,31 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
+import FormField from '../../../components/FormField';
 
 function CadastroCategoria() {
-  const [categorias, setCategorias] = useState(['TesteCategoria']);
-  
   const valoresIniciais = {
-    nome: 'Categoria Inicial',
-    descricao: 'Descricao inicial',
-    cor: '#000000',
+    nome: '',
+    descricao: '',
+    cor: '',
   }
+  const [categorias, setCategorias] = useState([]);
   const [values, setValues] = useState(valoresIniciais);
- 
+
+
   function setValue(chave, valor) {
-      // chave: nome, descricao, bla, bli
+    // chave: nome, descricao, bla, bli
     setValues({
       ...values,
       [chave]: valor, // nome: 'valor'
     })
+  }
+
+  function handleChange(infosDoEvento) {
+    setValue(
+      infosDoEvento.target.getAttribute('name'),
+      infosDoEvento.target.value
+    );
   }
 
   return (
@@ -25,65 +33,76 @@ function CadastroCategoria() {
       <h1>Cadastro de Categoria: {values.nome}</h1>
 
       <form onSubmit={function handleSubmit(infosDoEvento) {
-        infosDoEvento.preventDefault();
-        setCategorias([
-          ...categorias,
-          values
-        ]);
+          infosDoEvento.preventDefault();
+          setCategorias([
+            ...categorias,
+            values
+          ]);
+
+          setValues(valoresIniciais)
       }}>
-        
-      <div>
-        <label>
-          Nome da Categoria:
-          <input
-            type="text"
-            value={values.nome}
-            onChange={function funcaoHandlerQueOErroPediu(infosDoEvento) {
-              setValue('nome', infosDoEvento.target.value);
-            } }
-            />
-        </label>
-        </div>
 
-        <div>
-        <label>
-          Descrição:
-          <textarea
-            type="text"
-            value={values.descricao}
-            onChange={function funcaoHandlerQueOErroPediu(infosDoEvento) {
-              //setNomeDaCategoria(infosDoEvento.target.value);
-            } }
-            />
-        </label>
-        </div>
+        <FormField
+          label="Nome da Categoria"
+          type="text"
+          name="nome"
+          value={values.nome}
+          onChange={handleChange}
+        />
 
-        <div>
-        <label>
-          Cor:
-          <input
-            type="color"
-            value={values.cor}
-            onChange={function funcaoHandlerQueOErroPediu(infosDoEvento) {
-              //setNomeDaCategoria(infosDoEvento.target.value);
-            } }
+        <FormField
+          label="Descrição:"
+          type="????"
+          name="descricao"
+          value={values.descricao}
+          onChange={handleChange}
+        />
+        {/* <div>
+          <label>
+            Descrição:
+            <textarea
+              type="text"
+              value={values.descricao}
+              name="descricao"
+              onChange={handleChange}
             />
-        </label>
-        </div>
+          </label>
+        </div> */}
+
+        <FormField
+          label="Cor"
+          type="color"
+          name="cor"
+          value={values.cor}
+          onChange={handleChange}
+        />
+        {/* <div>
+          <label>
+            Cor:
+            <input
+              type="color"
+              value={values.cor}
+              name="cor"
+              onChange={handleChange}
+            />
+          </label>
+        </div> */}
 
         <button>
           Cadastrar
         </button>
       </form>
-        <ul>
-          {categorias.map((categoria, indice) => {
-            return (
-              <li key={`${categoria}${indice}`}>
-                {categoria}
-              </li>
-            )
-          })}
-        </ul>
+      
+
+      <ul>
+        {categorias.map((categoria, indice) => {
+          return (
+            <li key={`${categoria}${indice}`}>
+              {categoria.nome}
+            </li>
+          )
+        })}
+      </ul>
 
       <Link to="/">
         Ir para home
